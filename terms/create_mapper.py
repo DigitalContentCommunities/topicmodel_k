204768900533f7dd695ae88fbc16b84a8d986bb6
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2018-01-17 21:07:34 +0200

    Mapped version of data

---
+++ terms/create_mapper.py
@@ -0,0 +1,33 @@
+import xlrd
+book = xlrd.open_workbook("terms_all.xls")
+
+sh = book.sheet_by_index(0)
+
+import itertools
+
+## magic happens
+
+mapper = [
+    [],
+    [None] * 25,
+    [None] * 28,
+    [None] * 33,
+    [None] * 34
+ ]
+
+for c in range( 1, 5):
+    for r in range( sh.nrows ):
+
+        topic = sh.cell_value(rowx=r, colx=c)
+        if 'Topic' in topic:
+            topic = int( topic.split('Topic')[1] ) - 1 ## python indexing
+            label = sh.cell_value(rowx=r, colx=0)
+
+            mapper[c][topic] = label
+
+print "topics <- list()"
+
+for entry in mapper:
+
+    entry = map( lambda x: '"' + x + '"', entry )
+    print "topics[", len( entry ), "] = c(", ','.join( entry ) , ")"
